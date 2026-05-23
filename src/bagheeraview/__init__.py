ac3bf84bd5cd2e5340e0d16b9fddf6fa94942ded
Ignacio Serantes <kde@aynoa.net>
2026-05-23 07:31:43 +0200

    Revert version number to 1.0.0 and update license to GPLv3 with status set to Development

--- src/bagheeraview/__init__.py
+++ src/bagheeraview/__init__.py
@@ -3,11 +3,11 @@ Bagheera Viewer: a image viewer with KDE/Baloo.
 """
 
 __appname__ = "BagheeraView"
-__version__ = "1.1.0"
+__version__ = "1.0.0"
 __author__ = "Ignacio Serantes"
 __email__ = "kde@aynoa.net"
-__license__ = "LGPL"
-__status__ = "Production"
+__license__ = "GPLv3"
+__status__ = "Development"
 # "Prototype, Development, Alpha, Beta, Production, Stable, Deprecated"
 
 from .core.app import main
